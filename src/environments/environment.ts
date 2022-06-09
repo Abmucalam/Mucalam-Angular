d9a5e1a7e512c44cbd9f970abc999dba08cd2f89
Asnaira Mucalam <abmucalam@usm.edu.ph>
2022-06-09 20:17:50 +0800

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'https://mucalam-bookstore.netlify.app';
+
+export const environment = {
+  production: true,
+  application: {
+    baseUrl,
+    name: 'BookStore',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'http://amucalam-001-site1.etempurl.com',
+    redirectUri: baseUrl,
+    clientId: 'BookStore_App',
+    responseType: 'code',
+    scope: 'offline_access BookStore',
+    requireHttps: false,
+  },
+  apis: {
+    default: {
+      url: 'http://amucalam-001-site1.etempurl.com',
+      rootNamespace: 'Acme.BookStore',
+    },
+  },
+} as Environment;
